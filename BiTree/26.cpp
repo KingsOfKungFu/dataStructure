d657dd406c9a45034a7bd6309cd444a8f6109fd3
yanguang <18684965072@163.com>
2023-11-23 15:06:08 +0800

    11月23日复习

--- BiTree/26.cpp
+++ BiTree/26.cpp
@@ -2,9 +2,74 @@
 #include <iostream>
 #include "BiTreefun.cpp"
 using namespace std;
+#define maxsize 50
+
+void LevelWidth(BiTree T, int a[], int h){
+    if(T != NULL){
+        a[h] += 1;
+        LevelWidth(T->lchild,a,h+1);
+        LevelWidth(T->rchild,a,h+1);
+    }
+}
+
+int width(BiTree T){
+    int a[maxsize], h = 1;
+    for (int i = 0; i < maxsize; i++) {
+        a[i] = 0;
+    }
+    LevelWidth(T,a,h);
+    int wid = a[0];
+    for (int i = 1; i < maxsize; i++) {
+        if(a[i] > wid){
+            wid = a[i];
+        }
+    }
+    return wid;
+}
+
+typedef struct{
+    BTNode *p;
+    int lno;
+} St[maxsize];
+
+int width2(BTNode *boot){
+    St que;
+    int front = 0, rear = 0;
+    int Lno, i = 1, max = 0;
+    if(boot != NULL){
+        que[++rear].p = boot;
+        que[rear].lno = 1;
+        while(front != rear){
+            BTNode *q = que[++front].p;
+            Lno = que[front].lno;
+            if(q->lchild != NULL){
+                que[++rear].p = q->lchild;
+                que[rear].lno = Lno + 1;
+            }
+            if(q->rchild != NULL){
+                que[++rear].p = q->rchild;
+                que[rear].lno = Lno + 1;
+            }
+        }
+        while (i <= rear){
+            int n = 0;
+            int k = que[i].lno;
+            while (i <= rear && que[i].lno == k){
+                ++i;
+                ++n;
+            }
+            if(n > max){
+                max = n;
+            }
+        }
+    }
+    return max;
+}
 
 int main(){
     cout << "26、求解二叉树的宽度" << endl;
-
+    BTNode *T = aaaa();
+    printTree(T,0);
+    cout << "宽度为:" << width(T) << endl;
     return 0;
 }
--- BiTree/27.cpp
+++ BiTree/27.cpp
@@ -2,9 +2,39 @@
 #include <iostream>
 #include "BiTreefun.cpp"
 using namespace std;
+#define maxsize 50
+
+int depth(BTNode *boot){
+    if(boot == NULL){
+        return 0;
+    }
+    int front = 0, rear =0;
+    int last = 1, level = 0;
+    BTNode *q[maxsize];
+    q[++rear] = boot;
+    BTNode *p;
+    while (front < rear){
+        p = q[++front];
+        if(p->lchild){
+            q[++rear] = p->lchild;
+        }
+        if(p->rchild){
+            q[++rear] = p->rchild;
+        }
+        if(front == last){
+            level++;
+            last = rear;
+        }
+    }
+    return level;
+}
+
+
 
 int main(){
     cout << "27、用层次遍历求解二叉树的高度" << endl;
-
+    BTNode *T = aaaa();
+    printTree(T,0);
+    cout << "高度为:" << depth(T) << endl;
     return 0;
 }
